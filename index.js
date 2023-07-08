a0b50bdb947b2d3a7973ce50b868848e4bd53e7c
Chinnam-JanakiDevi <chinnam.janakidevi123@gmail.com>
2023-07-08 19:15:27 +0530

    backend to sql plus connection

---
+++ index.js
@@ -0,0 +1,21 @@
+const oracledb = require('oracledb');
+oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
+
+async function fun() {
+    let con;
+    try {
+        con = await oracledb.getConnection({
+            user: "system",
+            password: "manager",
+            connectString: "localhost:/orcl"
+        });
+        console.log("succesfully connected to oracle");
+        const data = await con.execute(
+            'SELECT * FROM sample',
+        );
+        console.log(data.rows);
+    } catch (err) {
+        console.error(err);
+    }
+}
+fun();
